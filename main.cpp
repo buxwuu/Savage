fa2c735f83af4c5679c24c3f457af4889bbb10f8
MugBergerFries <sjamesberger@gmail.com>
2017-11-15 20:59:36 -0700

    Spacing

--- main.cpp
+++ main.cpp
@@ -68,8 +68,7 @@ int login(){
 		getline(cin, username);
 		cout<<"Please enter your password"<<endl;
 		getline(cin, password);
-		pstmt=con->prepareStatement("SELECT player_ID FROM player_info WHERE username='"+username+"' AND 
-			password='"+password+"'");
+		pstmt=con->prepareStatement("SELECT player_ID FROM player_info WHERE username='"+username+"' AND password='"+password+"'");
 		res=pstmt->executeQuery();
 		if (!res->first()){
 			cout<<"User not found. Enter again or create account?\n1. Create account\n2. Enter again"<<endl;
@@ -103,8 +102,7 @@ int createUser(){
 		}
 		else{
 			delete pstmt;
-			pstmt=con->prepareStatement("INSERT INTO player_info (username, password) VALUES ('"+username+"', '"
-				+password+"')");
+			pstmt=con->prepareStatement("INSERT INTO player_info (username, password) VALUES ('"+username+"', '"+password+"')");
 			pstmt->executeUpdate();
 		}
 		delete pstmt;
@@ -124,16 +122,7 @@ void start(){
 
 case 1:
     update = "Let the game begin";
-    update2 = "A long time ago, before Tupac but not really before Tupac.  Luke Skywalker has returned to his 
-    	home planet of Tatooine in order to. . .you know what I don't remember, it's been to long since I've 
-    	seen the Star Wars movies.  I think he needs to find a way to defeat the Empire. . .which brings up a 
-    	funny question.  Isn't the Empire the good guys?  I mean they want law and order in the universe, while 
-    	Luke and his band of smugglers are trying to overthrow it.  Anyway maybe we got a little to over our heads 
-    	on this one. . .just please do me a favor and lower your expectations, we promise we'll make it up to you as 
-    	the game goes on.  We are all pressed for time in our other classes anyway, we had to dig hard for jokes.  
-    	Even the dude I saw bathing in the Boulder Creek River got to put a joke in here.  Anyway, not to keep you 
-    	waiting anymore, here is 'The Empire Strikes Back' starring Same Berger as Han Solo, Kevin Kirk as Darth Vader, 
-    	Hunter Haller as Chewbacca, Max Hayne as Luke Skywalker and Krishna Adettiwar as The Emperor.";
+    update2 = "A long time ago, before Tupac but not really before Tupac.  Luke Skywalker has returned to his home planet of Tatooine in order to. . .you know what I don't remember, it's been too long since I've seen the Star Wars movies.  I think he needs to find a way to defeat the Empire. . .which brings up a funny question.  Isn't the Empire the good guys?  I mean they want law and order in the universe, while Luke and his band of smugglers are trying to overthrow it.  Anyway maybe we got a little to over our heads on this one. . .just please do me a favor and lower your expectations, we promise we'll make it up to you as the game goes on.  We are all pressed for time in our other classes anyway, we had to dig hard for jokes.  Even the dude I saw bathing in the Boulder Creek River got to put a joke in here.  Anyway, not to keep you waiting anymore, here is 'The Empire Strikes Back' starring Same Berger as Han Solo, Kevin Kirk as Darth Vader, Hunter Haller as Chewbacca, Max Hayne as Luke Skywalker and Krishna Adettiwar as The Emperor.";
     cout << "\n"  << endl;
     stringupdate();
     system("read -p 'Press Enter to continue...' var");
