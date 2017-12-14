ebeb05a6b1ba183f29c672919ecd51cabe600f39
MugBergerFries <sjamesberger@gmail.com>
2017-12-14 00:33:22 -0700

    sql modified

--- createTestDB.sql
+++ createTestDB.sql
@@ -3,19 +3,17 @@ create database techtest;
 use techtest;
 
 create table if not exists player_info (
-	player_ID int(4) not null,	
+	player_ID int(4) primary key not null auto_increment,	
 	username char(20) not null,
 	password char(20) not null,
 	progress int not null default 0,
     luke int not null default 0,
-    obiwan int not null default 0,
-    primary key (player_ID)
+    obiwan int not null default 0
 );
 
 create table if not exists inventory (
-	player_ID int(4) not null,
+	player_ID int(4) primary key not null auto_increment,
 	blaster int not null default 0,
 	lightsaber int not null default 0,
-	grenade int not null default 0,
-	primary key (player_ID)
+	grenade int not null default 0
 );
