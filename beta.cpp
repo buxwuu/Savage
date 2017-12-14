6088d903f400b1a826270e940dc8a3af5ee03af0
MugBergerFries <sjamesberger@gmail.com>
2017-12-14 14:58:39 -0700

    added cout sleeps to whole program

--- beta.cpp
+++ beta.cpp
@@ -204,31 +204,31 @@ void start(){
     	    cout<<"Let the game begin"<<endl<<endl;
         	usleep(500000);
     	    cout<<"A long time ago, before Tupac but not really before Tupac"<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
     	    cout<<"Luke Skywalker has returned to his home planet of Tatooine in order to. . ."<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
     	    cout<<"You know what I don't remember, it's been too long since i've seen the Star Wars movies"<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
     	    cout<<"I think he needs to find a way to defeat the Empire. . .which brings up a funny question"<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
     	    cout<<"Isn't the Empire the good guys?  I mean they want law and order in the universe"<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
     	    cout<<"While Luke and his band of smugglers are trying to overthrow it"<<endl;
-        	usleep(1500000);
-    	    cout<<"Anyway maybe we got a little to over our heads n this one. . ."<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
+    	    cout<<"Anyway maybe we got a little to over our heads on this one. . ."<<endl;
+        	usleep(1250000);
     	    cout<<"Just please do me a favor and lower your expectations"<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
     	    cout<<"We promise we'll make it up to you as the game goes on"<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
     	    cout<<"We are all pressed for time in our other classes anyway, we had to dig hard for jokes"<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
     	    cout<<"Even the dude I saw bathing in the Boulder Creek River got to put a joke in here"<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
     	    cout<<"Anyway, not to keep you waiting anymore, here is 'The Empire Strikes Back' starring Sam Berger as Han Solo"<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
     	    cout<<"Kevin Kirk as Darth Vader, Hunter Haller as Chewbacca, Max Hayne as Luke Skywalker"<<endl;
-        	usleep(1500000);
+        	usleep(1250000);
     	    cout<<"And Krishna Adettiwar as The Emperor"<<endl<<endl;
     	    cout<<string(100,'\n');
         	usleep(1);
@@ -260,8 +260,13 @@ void Tatooine(){
     delete res;
     delete pstmt;
     here:
-    cout<<"To the west, you see a dome-like building\nTo the east, you see a mountain region with a small cave entrance built in"<<endl;
+	usleep(750000);
+    cout<<"To the west, you see a dome-like building"<<endl;
+	usleep(750000);
+    cout<<"To the east, you see a mountain region with a small cave entrance built in"<<endl;
+	usleep(750000);
     cout<<"To the north, you see a worn-down home structure\n"<<endl;
+	usleep(750000);
     if (luke==0)cout<<"1. Go to the dome-like building"<<endl;
     cout<<"2. Go to the cave"<<endl;
     if (grenade==0)cout<<"3. Go to the worn-down home"<<endl;
@@ -303,18 +308,29 @@ void Tatooine(){
 }
 
 void Lukeshome(){
-    cout<<"As you enter the building, you see a man you immediately recognize as Luke Skywalker meditating on the floor\nHe looks up at you as you enter\n";
+	usleep(750000);
+    cout<<"As you enter the building, you see a man you immediately recognize as Luke Skywalker meditating on the floor"<<endl;
+	usleep(750000);
+    cout<<"He looks up at you as you enter"<<endl;
+	usleep(750000);
     cout<<"Luke - Ah, you're the one they've sent to help me, glad to have you!"<<endl;
-    cout << "I have lost my lightsaber and I need you to recover it for me" << endl;
+	usleep(750000);
+    cout<<"I have lost my lightsaber and I need you to recover it for me" << endl;
+	usleep(750000);
     cout<<"I was jumped by some smugglers while bulls-eyeing womp-rats in my T-16. I think they are set up in that cave by the mountain"<<endl;
+	usleep(750000);
     cout<<"Here, take my spare blaster, you might need it!"<<endl;
+	usleep(750000);
     pstmt=con->prepareStatement("UPDATE inventory SET blaster = 1 WHERE player_ID = "+userstr);
     pstmt->executeUpdate();
     delete pstmt;
     cout<<"BLASTER ACQUIRED\n"<<endl;
     system("read -p 'Press Enter to continue...' var");
+	usleep(250000);
     cout<<"If you haven't already, you may want to visit Obi-Wan at his home, he might be able to supply you with other gear as well"<<endl;
+	usleep(750000);
     cout<<"Either way, may the Force be with you"<<endl;
+	usleep(750000);
     cout<<"You leave the dwelling\n"<<endl;
     pstmt=con->prepareStatement("UPDATE player_info SET luke = 1 WHERE player_ID = "+userstr);
     pstmt->executeUpdate();
@@ -338,12 +354,23 @@ void obiwan(){
     obi = res->getInt("obiwan");
     delete res;
     delete pstmt;
-    cout<<"You arrive at the small hut. Pushing the ragged flap covering the entrance aside, you enter...\n";
-    cout<<"An old man sits at a table in the center of the dwelling\nhe looks up at you as you enter\n";
+	usleep(750000);
+    cout<<"You arrive at the small hut. Pushing the ragged flap covering the entrance aside, you enter..."<<endl;
+	usleep(750000);
+    cout<<"An old man sits at a table in the center of the dwelling"<<endl;
+	usleep(750000);
+    cout<<"He looks up at you as you enter"<<endl;
+	usleep(750000);
     if (obi==0){
         cout<<"Obi-Wan - Ah, a visitor! How can I help you?\n";
+    	usleep(750000);
         cout<<"You - I've come to help Luke Skywalker, can you assist?\n";
-        if (x==0) cout<<"Obi-Wan - He lives in the dome-shaped building nearby\nObi-Wan - If you prove yourself worthy, I can also give you a handy piece of gear\n\n";
+    	usleep(750000);
+        if (x==0){
+        	cout<<"Obi-Wan - He lives in the dome-shaped building nearby"<<endl;
+        	usleep(750000);
+        	cout<<"Obi-Wan - If you prove yourself worthy, I can also give you a handy piece of gear\n"<<endl;
+        } 
         else cout<<"Obi-Wan - If you prove yourself worthy, I can give you a handy piece of gear\n\n";
     }
     else cout<<"Obi-Wan - You're back, change your mind on that challenge?"<<endl;
@@ -362,24 +389,36 @@ void obiwan(){
     input = instr - '0';
     switch (input){
         case 1:
+        	usleep(750000);
             cout<<"Obi-Wan - If you can answer this question correct, I'll give you what you need"<<endl;
+        	usleep(750000);
             cout<<"Obi-Wan - This should be simple for a rebel soldier like you"<<endl;
-            cout<<"Obi-Wan - What was the name of the Star Destroyer that Darth Vader commanded?"<<endl;
+        	usleep(750000);
+            cout<<"Obi-Wan - What was the name of the Star Destroyer that Darth Vader commanded?"<<endl<<"You - ";
             getline(cin, guess);
             if (guess=="Executor"||guess=="executor"||guess=="EXECUTOR"){
+        		usleep(250000);
                 cout<<"Obi-Wan - Indeed. It was the most fearsome Star Destroyer I've ever come across"<<endl;
-                cout<<"Obi-Wan - Well, I suppose I owe you this\nHe hands you a small sphere\nGRENADE ACQUIRED"<<endl;
+	        	usleep(750000);
+                cout<<"Obi-Wan - Well, I suppose I owe you this"<<endl;
+    	    	usleep(750000);
+                cout<<"He hands you a small sphere"<<endl;
+        		usleep(750000);
+                cout<<"GRENADE ACQUIRED"<<endl;
                 pstmt=con->prepareStatement("UPDATE inventory SET grenade = 1 WHERE player_ID = "+userstr);
                 pstmt->executeUpdate();
                 delete pstmt;
             }
             else{
+        		usleep(750000);
                 cout<<"Obi-Wan - Incorrect, I'm afraid"<<endl;
+        		usleep(750000);
                 cout<<"I'm afraid I must ask you to leave now"<<endl;
             }
             pstmt=con->prepareStatement("UPDATE player_info SET obiwan = 1 WHERE player_ID = "+userstr);
             pstmt->executeUpdate();
             delete pstmt;
+        	usleep(750000);
             cout<<"You leave the old man's dwelling\n"<<endl;
             Tatooine();
             break;
@@ -387,16 +426,28 @@ void obiwan(){
             pstmt=con->prepareStatement("UPDATE player_info SET obiwan = 1 WHERE player_ID = "+userstr);
             pstmt->executeUpdate();
             delete pstmt;
-            cout<<"Obi-Wan - May the Force be with you, return if you wish\nYou exit the dwelling\n"<<endl;
+        	usleep(250000);
+            cout<<"Obi-Wan - May the Force be with you, return if you wish"<<endl;
+        	usleep(750000);
+            cout<<"You exit the dwelling\n"<<endl;
             Tatooine();
             break;
         case 3:
             if (x==0){
+        		usleep(200000);
                 cout<<"INVALID OPTION"<<endl;
+        		usleep(200000);
                 goto here;
             }
-            cout<<"Obi-Wan - Very well\nHe hands over a small sphere\nGRENADE ACQUIRED"<<endl;
+        	usleep(750000);
+            cout<<"Obi-Wan - Very well"<<endl;
+        	usleep(750000);
+            cout<<"He hands over a small sphere"<<endl;
+        	usleep(200000);
+            cout<<"GRENADE ACQUIRED"<<endl;
+        	usleep(750000);
             cout<<"Obi-Wan - Now leave my home, and don't come back"<<endl;
+        	usleep(750000);
             pstmt=con->prepareStatement("UPDATE inventory SET grenade = 1 WHERE player_ID = "+userstr);
             pstmt->executeUpdate();
             delete pstmt;
@@ -409,10 +460,13 @@ void obiwan(){
             pstmt=con->prepareStatement("UPDATE player_info SET progress = 2 WHERE player_ID = "+userstr);
             pstmt->executeUpdate();
             delete pstmt;
+        	usleep(200000);
             cout<<"Your progress has been saved, thanks for playing!"<<endl;
             break;
         default:
+        	usleep(200000);
             cout<<"INVALID OPTION"<<endl;
+        	usleep(200000);
             goto here;
             break;
     }
@@ -421,9 +475,13 @@ void obiwan(){
 void randomcave(){
     srand(time(NULL));
     int chance;
+	usleep(750000);
     cout<<"\nYou enter the cave, and spot 5 smugglers gathered around a campfire"<<endl;
+	usleep(750000);
     cout<<"Unfortunately, they seem to have spotted you too!"<<endl;
+	usleep(750000);
     cout<<"Smuggler - You've messed up, boy! *draws blaster*"<<endl;
+	usleep(750000);
     pstmt=con->prepareStatement("SELECT * FROM inventory WHERE player_ID = "+userstr);
     res=pstmt->executeQuery();
     res->first();
@@ -456,15 +514,23 @@ void randomcave(){
             system("read -p 'Press Enter to continue...' var");
             if (chance>74){
                 cout<<"You charge the smugglers, yelling wildly with your fists raised"<<endl;
-                cout<<"You charge catches them off guard, you smash the first smugglers nose with a nice right hook"<<endl;
+	        	usleep(750000);
+	            cout<<"You charge catches them off guard, you smash the first smugglers nose with a nice right hook"<<endl;
+    	    	usleep(750000);
                 cout<<"He goes down and you grab his blaster"<<endl;
+        		usleep(750000);
                 cout<<"Using his body as a shield, you snap off two shots, taking down the closest smugglers"<<endl;
+        		usleep(750000);
                 cout<<"The survivors flee"<<endl;
             }
             else{
                 cout<<"You charge the smugglers, yelling wildly with your fists raised"<<endl;
+	        	usleep(750000);
                 cout<<"The one who spoke shoots you down, laughing with his friends as your vision goes dark"<<endl;
-                cout<<"YOU HAVE DIED\nGAME OVER"<<endl;
+    	    	usleep(750000);
+                cout<<"YOU HAVE DIED"<<endl;
+        		usleep(250000);
+                cout<<"GAME OVER"<<endl;
                 pstmt=con->prepareStatement("UPDATE inventory SET blaster = 0, lightsaber = 0, grenade = 0 WHERE player_ID = "+userstr);
                 pstmt->executeUpdate();
                 delete pstmt;
@@ -479,14 +545,20 @@ void randomcave(){
                 system("read -p 'Press Enter to continue...' var");
                 if (chance>49){
                     cout<<"You pull your blaster and take cover behind a nearby boulder"<<endl;
+		        	usleep(750000);
                     cout<<"A blaster shot hits your shoulder, wounding your arm"<<endl;
+        			usleep(750000);
                     cout<<"You fire off a few shots, taking down three of the smugglers"<<endl;
+		        	usleep(750000);
                     cout<<"The remaining 2 smugglers flee"<<endl;
                 }
                 else{
                     cout<<"You dive behind the nearest boulder"<<endl;
+        			usleep(750000);
                     cout<<"You pull your blaster, only managing to take down one smuggler before the others shoot you down"<<endl;
+		        	usleep(750000);
                     cout<<"YOU HAVE DIED"<<endl;
+        			usleep(750000);
                     cout<<"GAME OVER"<<endl;
                     pstmt=con->prepareStatement("UPDATE inventory SET blaster = 0, lightsaber = 0, grenade = 0 WHERE player_ID = "+userstr);
                     pstmt->executeUpdate();
@@ -498,7 +570,9 @@ void randomcave(){
                 }
             }
             else{
+    			usleep(200000);
                 cout<<"INVALID OPTION"<<endl;
+        		usleep(200000);
                 goto here;
             }
             break;
@@ -508,16 +582,24 @@ void randomcave(){
                 system("read -p 'Press Enter to continue...' var");
             	if (chance>74){
             		cout<<"Reacting fast, you press the detonation button on the grenade"<<endl;
+		        	usleep(750000);
             		cout<<"You underhand toss it right into the center of the large group"<<endl;
+        			usleep(750000);
             		cout<<"Diving behind the closest boulder, you dodge a few panicked laser shots"<<endl;
+		        	usleep(750000);
             		cout<<"With a deafening bang, the grenade explodes"<<endl;
+        			usleep(750000);
             		cout<<"Peeking out from behind the boulder, you see four smugglers dead, the other has fled"<<endl;
             	}
             	else{
             		cout<<"You fumble for the grenade in your pack"<<endl;
+		        	usleep(750000);
             		cout<<"Pulling it out, you just manage to arc your arm back before the smugglers fire"<<endl;
+        			usleep(750000);
             		cout<<"As you hit the ground, the light starts to fade"<<endl;
+		        	usleep(750000);
                     cout<<"YOU HAVE DIED"<<endl;
+        			usleep(750000);
                     cout<<"GAME OVER"<<endl;
                     pstmt=con->prepareStatement("UPDATE inventory SET blaster = 0, lightsaber = 0, grenade = 0 WHERE player_ID = "+userstr);
                     pstmt->executeUpdate();
@@ -529,18 +611,24 @@ void randomcave(){
             	}
             }
             else{
+        		usleep(200000);
                 cout<<"INVALID OPTION"<<endl;
+        		usleep(200000);
                 goto here;
             }
             break;
         case 4:
         	if (grenade==1 && blaster==1){
         		cout<<"With lightning speed, you arm the grenade and toss it into the group of smugglers"<<endl;
+	        	usleep(750000);
         		cout<<"Before they even have a chance to react, you take cover behind the nearest boulder"<<endl;
+    	    	usleep(750000);
         		cout<<"You gun down those smugglers who were quick enough to even try to run"<<endl;
         	}
         	else{
+        		usleep(200000);
                 cout<<"INVALID OPTION"<<endl;
+        		usleep(200000);
                 goto here;
         	}
             break;
@@ -548,22 +636,28 @@ void randomcave(){
             pstmt=con->prepareStatement("UPDATE player_info SET progress = 3 WHERE player_ID = "+userstr);
             pstmt->executeUpdate();
             delete pstmt;
+        	usleep(200000);
             cout<<"Your progress has been saved, thanks for playing!"<<endl;
             break;
         default:
+        	usleep(200000);
             cout<<"INVALID OPTION"<<endl;
+        	usleep(200000);
             goto here;
         	break;
     }
     system("read -p 'Press Enter to continue...' var");
     cout<<"You search the bodies, finding Luke's lightsaber on the first smuggler's corpse"<<endl;
+	usleep(500000);
     cout<<"LIGHTSABER ACQUIRED"<<endl;
     pstmt=con->prepareStatement("UPDATE inventory SET lightsaber = 1 WHERE player_ID = "+userstr);
     pstmt->executeUpdate();
     delete pstmt;
     system("read -p 'Press Enter to continue...' var");
     cout<<"YOU WIN"<<endl;
+	usleep(250000);
     cout<<"CONGRATULATIONS"<<endl;
+	usleep(750000);
     cout<<"GAME OVER"<<endl;
     pstmt=con->prepareStatement("UPDATE inventory SET blaster = 0, lightsaber = 0, grenade = 0 WHERE player_ID = "+userstr);
     pstmt->executeUpdate();
